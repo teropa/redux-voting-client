0cfbad0da233cb967fd3e1e71f8ce840a8e085a2
Tero Parviainen <tero@teropa.info>
2015-09-05 12:49:20 +0300

    state from socket to redux

--- src/index.jsx
+++ src/index.jsx
@@ -9,17 +9,11 @@ import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state: state})
+);
 
 const routes = <Route handler={App}>
   <Route path="/results" handler={ResultsContainer} />
