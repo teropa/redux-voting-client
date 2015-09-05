dd8b33027ef528508491bb813c9f93ca426bc460
Tero Parviainen <tero@teropa.info>
2015-09-05 11:20:05 +0300

    socket.io connection

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react/addons';
 import Router, {Route, DefaultRoute} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
@@ -18,7 +19,8 @@ store.dispatch({
   }
 });
 
->>>>>>> bed60b8... reducer and redux store
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+
 const routes = <Route handler={App}>
   <Route path="/results" handler={ResultsContainer} />
   <DefaultRoute handler={VotingContainer} />
